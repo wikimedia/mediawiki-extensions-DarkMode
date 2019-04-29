4eff03632c1973200d549d77871a78951a60c5b0
MusikAnimal <musikanimal@gmail.com>
2019-04-28 22:08:12 -0400

    Initial draft for DarkMode extension
    This includes just the basics. A link for "Dark mode" is added to the
    personal toolbar, but this is not a final design, nor is it actually
    functional. Actual functionality to come with a subsequent patch.
    
    Bug: T221877
    Change-Id: I89d9c17a701221c31bd6c4180dce038f1936fc06

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+/* eslint-env node */
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				reportUnusedDisableDirectives: true,
+				cache: true
+			},
+			all: [
+				'**/*.js',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		stylelint: {
+			options: {
+				syntax: 'less'
+			},
+			all: [
+				'**/*.{css,less}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		banana: conf.MessagesDirs,
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'jsonlint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
---
+++ includes/Hooks.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MediaWiki\Extension\DarkMode;
+
+use SkinTemplate;
+use Title;
+
+class Hooks {
+	/**
+	 * Handler for PersonalUrls hook.
+	 * Add a "Dark mode" item to the user toolbar ('personal URLs').
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/PersonalUrls
+	 * @param array &$personal_urls Array of URLs to append to.
+	 * @param Title &$title Title of page being visited.
+	 * @param SkinTemplate $skin
+	 */
+	public static function onPersonalUrls( array &$personal_urls, Title &$title, SkinTemplate $skin ) {
+		$insertAfter = $skin->getUser()->isAnon() ? 'anontalk' : 'mytalk';
+		$insertUrls = [
+			'darkmode-link' => [
+				'text' => $skin->msg( 'darkmode-link' )->text(),
+				'href' => '#',
+				'active' => false,
+			]
+		];
+
+		$personal_urls = wfArrayInsertAfter( $personal_urls, $insertUrls, $insertAfter );
+	}
+}
