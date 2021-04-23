a4b2ada28e9cfca6b8bf139d25fb89130e4b28b9
r4356th <radman.siddiki@gmail.com>
2021-04-23 19:54:33 +0600

    Make DarkMode usable by anons and add user pref
    Moves "Dark mode" link to the footer to make it usable by everyone.
    Adds a URL parameter that allows toggling dark mode for a specific page.
    Adds a new user preference that allows toggling dark mode on all pages.
    
    Bug: T241925
    Change-Id: I12b92499f64860d921928496ee1b975e3982bb3f
    (cherry picked from commit 0224fa679de8bef0fddadb46e1b4b25550f6b0f5)

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,34 +2,29 @@
 
 namespace MediaWiki\Extension\DarkMode;
 
+use Html;
+use MediaWiki\MediaWikiServices;
 use OutputPage;
 use Skin;
-use SkinTemplate;
-use Title;
+use User;
 
 class Hooks {
 	/**
-	 * Handler for PersonalUrls hook.
-	 * Add a "Dark mode" item to the user toolbar ('personal URLs').
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/PersonalUrls
-	 * @param array &$personal_urls Array of URLs to append to.
-	 * @param Title &$title Title of page being visited.
-	 * @param SkinTemplate $skin
+	 * Handler for SkinAddFooterLinks hook.
+	 * Add a "Dark mode" item to the footer.
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/SkinAddFooterLinks
+	 * @param Skin $skin Skin being used.
+	 * @param string $key Current position in the footer.
+	 * @param array &$footerlinks Array of URLs to add to.
 	 */
-	public static function onPersonalUrls( array &$personal_urls, Title &$title, SkinTemplate $skin ) {
+	public static function onSkinAddFooterLinks( Skin $skin, string $key, array &$footerlinks ) {
 		if ( !self::shouldHaveDarkMode( $skin ) ) {
 			return;
 		}
 
-		$insertUrls = [
-			'darkmode-link' => [
-				'text' => $skin->msg( 'darkmode-link' )->text(),
-				'href' => '#',
-				'active' => false,
-			]
-		];
-
-		$personal_urls = wfArrayInsertAfter( $personal_urls, $insertUrls, 'mytalk' );
+		if ( $key === 'places' ) {
+			$footerlinks['darkmode-link'] = Html::element( 'a', [ 'href' => '#' ], $skin->msg( 'darkmode-link' )->text() );
+		}
 	}
 
 	/**
@@ -45,6 +40,14 @@ class Hooks {
 
 		$output->addModules( 'ext.DarkMode' );
 		$output->addModuleStyles( 'ext.DarkMode.styles' );
+
+		$req = $output->getRequest();
+		$user = $skin->getUser();
+		if ( $req->getVal( 'usedarkmode' ) ) {
+			self::toggleDarkMode( $output );
+		} elseif ( MediaWikiServices::getInstance()->getUserOptionsLookup()->getBoolOption( $user, 'darkmode' ) ) {
+			self::toggleDarkMode( $output );
+		}
 	}
 
 	/**
@@ -53,6 +56,28 @@ class Hooks {
 	 * @return bool
 	 */
 	private static function shouldHaveDarkMode( Skin $skin ) {
-		return $skin->getUser()->isLoggedIn() && $skin->getSkinName() !== 'minerva';
+		return $skin->getSkinName() !== 'minerva';
+	}
+
+	/**
+	 * Allow others to toggle Dark Mode
+	 * @param OutputPage $output
+	 */
+	public static function toggleDarkMode( OutputPage $output ) {
+		$output->addHtmlClasses( 'client-dark-mode' );
+	}
+
+	/**
+	 * Handler for GetPreferences hook
+	 * Add hidden preference to keep dark mode turned on all pages
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/GetPreferences
+	 * @param User $user Current user
+	 * @param array &$preferences
+	 */
+	public static function onGetPreferences( User $user, array &$preferences ) {
+		$preferences['darkmode'] = [
+			'type' => 'api',
+			'default' => 0,
+		];
 	}
 }
