e56ef5ac1377fc468c73c4dd165ef3456b240e8e
r4356th <radman.siddiki@gmail.com>
2021-04-23 19:54:33 +0600

    Make DarkMode usable by anons and add user pref
    Moves "Dark mode" link to the footer to make it usable by everyone.
    Adds a URL parameter that allows toggling dark mode for a specific page.
    Adds a new user preference that allows toggling dark mode on all pages.
    
    Bug: T241925
    Change-Id: I12b92499f64860d921928496ee1b975e3982bb3f
    (cherry picked from commit 0224fa679de8bef0fddadb46e1b4b25550f6b0f5)

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -1,11 +1,16 @@
 $( function () {
-	var darkMode = false;
+	var darkMode = document.documentElement.classList.contains( 'client-dark-mode' );
 
-	$( '#pt-darkmode-link a' ).on( 'click', function ( e ) {
+	function updatetext() {
+		$( '#footer-places-darkmode-link a' ).text( mw.msg( darkMode ? 'darkmode-default-link' : 'darkmode-link' ) );
+	}
+	$( updatetext() );
+	$( '#footer-places-darkmode-link a' ).on( 'click', function ( e ) {
 		e.preventDefault();
 		darkMode = !darkMode;
 
 		$( document.documentElement ).toggleClass( 'client-dark-mode', darkMode );
-		$( e.target ).text( mw.msg( darkMode ? 'darkmode-default-link' : 'darkmode-link' ) );
+		updatetext();
+		new mw.Api().saveOption( 'darkmode', darkMode ? 1 : 0 );
 	} );
 } );
