1da37855a641e0388e9042f7f96f544d7286a5a6
Fomafix <fomafix@googlemail.com>
2021-10-25 20:01:35 +0000

    Use addBodyClasses instead of addHtmlClasses
    addBodyClasses is the prefered way to add classes for styling the
    document body.
    
    This change moves the class `client-dark-mode` from the <html> element
    to the <body> element. Therefor the selector `.client-dark-mode body`
    in ext.DarkMode.less doesn't match anymore. So the styling for the
    <body> element is now directly in the selector `.client-dark-mode`.
    
    Change-Id: Ic27059c11635c3bcd957de6863a4bacb012ac8f2

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -2,7 +2,7 @@ $( function () {
 	$( '.darkmode-link' ).on( 'click', function ( e ) {
 		e.preventDefault();
 
-		var darkMode = document.documentElement.classList.toggle( 'client-dark-mode' );
+		var darkMode = document.body.classList.toggle( 'client-dark-mode' );
 
 		$( this ).text( mw.msg( darkMode ? 'darkmode-default-link' : 'darkmode-link' ) );
 		new mw.Api().saveOption( 'darkmode', darkMode ? 1 : 0 );
