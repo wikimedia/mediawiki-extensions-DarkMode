e35ceaa79eddd7d43a10e1eea7abda0dc4b43888
MusikAnimal <musikanimal@gmail.com>
2022-07-31 16:15:03 -0400

    Add support for Vector 2022 and update dark mode styles
    * In Vector 2022, the link now uses the 'moon' and 'bright' icons from
      the OOUI accessibility icon set
    * Sync CSS from enwiki gadget
    * Add tooltips to the links
    * Use 'ext-darkmode-link' as the selector, per convention
    * Refactor and cleanup the Hooks class
    * Update JS to use ES6, since the CSS isn't supported by IE anyways
    * Bump to version 0.2.0 and set MW requirement to >= 1.38.0
    
    Change-Id: I454b0d7060818afb8c9c4635fd82e24de6de1d76

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -1,10 +1,37 @@
-$( function () {
-	$( '.darkmode-link' ).on( 'click', function ( e ) {
+/**
+ * Some code adapted from the enwiki gadget https://w.wiki/5Ktj
+ */
+$( () => {
+	const $darkModeLink = $( '.ext-darkmode-link' );
+
+	$darkModeLink.on( 'click', ( e ) => {
 		e.preventDefault();
 
-		var darkMode = document.body.classList.toggle( 'client-dark-mode' );
+		const darkMode = document.body.classList.toggle( 'client-darkmode' );
 
-		$( this ).text( mw.msg( darkMode ? 'darkmode-default-link' : 'darkmode-link' ) );
-		new mw.Api().saveOption( 'darkmode', darkMode ? 1 : 0 );
+		// Update the icon.
+		if ( darkMode ) {
+			$darkModeLink.find( '.mw-ui-icon-moon' )
+				.removeClass( 'mw-ui-icon-moon' )
+				.addClass( 'mw-ui-icon-bright' );
+		} else {
+			$darkModeLink.find( '.mw-ui-icon-bright' )
+				.removeClass( 'mw-ui-icon-bright' )
+				.addClass( 'mw-ui-icon-moon' );
+		}
+
+		// Use different CSS selectors for the dark mode link based on the skin.
+		// eslint-disable-next-line no-restricted-syntax
+		const labelSelector = [ 'vector', 'vector-2022', 'minerva' ].includes( mw.config.get( 'skin' ) ) ?
+			'span:not(.mw-ui-icon)' :
+			'a';
+
+		// Update the link text.
+		$darkModeLink.find( labelSelector )
+			.text( mw.msg( darkMode ? 'darkmode-default-link' : 'darkmode-link' ) );
+		new mw.Api().saveOption( 'darkmode', darkMode ? '1' : '0' );
+
+		// Update the mobile theme-color
+		$( 'meta[name="theme-color"]' ).attr( 'content', darkMode ? '#000000' : '#eaecf0' );
 	} );
 } );
